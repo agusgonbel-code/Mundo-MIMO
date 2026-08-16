592115686cd9d9ce16bb35e64ce1c9abb2c8c8db
agusgonbel-code <agusgonbel@gmail.com>
2026-08-16 08:23:50 +0200

    Sprint: cache Lio and Pipa characters

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mundo-mimo-v5';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/mimo-bear.svg'];
+const CACHE='mundo-mimo-v6';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/mimo-bear.svg','./assets/lio-lion.svg','./assets/pipa-elephant.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
